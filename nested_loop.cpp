f3722fa3a885e54d66e4b9855b1715197c3cadce
Ayu Aulia x rpl 2 <113896961+Ayuaulia06@users.noreply.github.com>
2022-11-28 14:51:06 +0700

    Add files via upload

---
+++ nested_loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main(){
+  int a,b,c;
+  for(a=1;a<=10;a++){
+    for(b=1;b<=a;b++){
+      printf("%i",a);
+    }
+  printf("\n");
+  }
+}
