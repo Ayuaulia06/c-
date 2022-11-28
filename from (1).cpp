cb120cebf41894a285e8efaa0df5a0f3f5224775
Ayu Aulia x rpl 2 <113896961+Ayuaulia06@users.noreply.github.com>
2022-11-28 14:55:17 +0700

    Add files via upload

---
+++ from (1).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+  int nilai =78;
+  if (nilai >78){
+    } else if (nilai ==78){
+       cout << "lulus kkm";
+    } else {
+        cout << " tidak lulus ";
+    }
+    return 0;
+    }
+    
